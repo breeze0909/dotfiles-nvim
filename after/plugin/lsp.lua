608769a70302d1ce4df57f48ce0b86ec91939608
breeze0909@gmail.com <brmon0923@gmail.com>
2024-04-30 16:08:38 +0000

    dotfiles

---
+++ after/plugin/lsp.lua
@@ -0,0 +1,19 @@
+local lsp = require('lsp-zero')
+
+lsp.on_attach(function(client, bufnr)
+  -- see :help lsp-zero-keybindings
+  -- to learn the available actions
+  lsp.default_keymaps({buffer = bufnr})
+end)
+
+require('mason').setup({})
+require('mason-lspconfig').setup({
+  -- Replace the language servers listed here 
+  -- with the ones you want to install
+  ensure_installed = {'tsserver', 'rust_analyzer'},
+  handlers = {
+    function(server_name)
+      require('lspconfig')[server_name].setup({})
+    end,
+  },
+})
